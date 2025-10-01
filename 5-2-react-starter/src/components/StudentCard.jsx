a70da597cd8dc4e09c3b9fb16db66d175a74b4ec
darkwinTech <s202257160@kfupm.edu.sa>
2025-10-01 11:48:37 +0300

    T1

---
+++ 5-2-react-starter/src/components/StudentCard.jsx
@@ -0,0 +1,10 @@
+function StudentCard() {
+    return (
+        <div>
+            <h3> Name: Aleen AlQarni</h3>
+            <p> ID: 202257160 </p>
+            <p> Department: Information and Computer Science</p>
+        </div>
+    )
+}
+export default StudentCard;
